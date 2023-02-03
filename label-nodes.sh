08e6acbae5e0a7b2bcb3a89d2a3026e3aa65e469
Kevin Haller <contact@kevinhaller.dev>
2023-02-03 19:56:39 +0100

    Introduces a new script for labelling the nodes.

---
+++ label-nodes.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+set -x
+set -e
+
+USER=${1:-neo4j}
+PASSWORD=${2:-neo4j}
+
+echo "> Label all the nodes in the graph with DBI."
+
+QUERY_FILE=$(mktemp)
+
+BATCH_SIZE=10000
+
+cat > $QUERY_FILE << EOL
+MATCH (n)
+WITH count(n) AS totalNodes, ${BATCH_SIZE}
+
+WITH range(0, totalNodes, ${BATCH_SIZE}) AS batchStarts
+UNWIND batchStarts as batchStart
+
+MATCH (x:Resource)
+WHERE id(x) >= batchStart AND id(x) < batchStart + ${BATCH_SIZE} AND x.uri STARTS WITH 'http://dbpedia.org'
+SET x:DBI
+RETURN null
+EOL
+
+echo "> Labelling query was sent to Neo4J."
+cypher-shell --file $QUERY_FILE --user "$USER" --password "$PASSWORD"
+
+rm -rf $QUERY_FILE
+
+echo "> Finished labelling of nodes."
