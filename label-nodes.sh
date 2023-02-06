536ba3a3485ab704e5c9ac528da9cc2711dcbfe0
Kevin Haller <contact@kevinhaller.dev>
2023-02-06 13:49:54 +0100

    Refactoring the script for labelling nodes. Other labelling methods can be added in the future.

--- label-nodes.sh
+++ label-nodes.sh
@@ -2,26 +2,35 @@
 set -x
 set -e
 
-USER=${1:-neo4j}
-PASSWORD=${2:-neo4j}
-
-echo "> Label all the nodes in the graph with DBI."
-
-QUERY_FILE=$(mktemp)
-
 BATCH_SIZE=100000
 
-cat > $QUERY_FILE << EOL
+dbi_labelling() {
+  cat > $1 << EOL
 CALL apoc.periodic.iterate(
   "MATCH (x:Resource) WHERE x.uri STARTS WITH 'http://dbpedia.org' RETURN x",
   "SET x:DBI",
   {batchSize:${BATCH_SIZE}, parallel:true}
 )
 EOL
+}
 
-echo "> Labelling query was sent to Neo4J."
+METHOD=$1
+USER=${2:-neo4j}
+PASSWORD=${3:-neo4j}
+
+echo ">($METHOD)> Started to label nodes."
+
+QUERY_FILE=$(mktemp)
+
+if [ "$METHOD" = "dbi" ]; then
+  dbi_labelling $QUERY_FILE
+else
+  echo "unknown method '$METHOD' specified."
+  exit 1
+fi
+
+echo ">($METHOD)> Labelling query was sent to Neo4J."
 cypher-shell --file $QUERY_FILE --user "$USER" --password "$PASSWORD"
 
+echo ">($METHOD)> Finished labelling of nodes."
 rm -rf $QUERY_FILE
-
-echo "> Finished labelling of nodes."
